3a58384e520e14bd52507cee5836e19f00ff670f
Steve Saju <stevesajustephen@gmail.com>
2026-01-15 17:17:45 +0100

    update test

--- test/launcher.ts
+++ test/launcher.ts
@@ -4,13 +4,13 @@ import { handler } from "../src/services/climate-ledger/handler";
 handler(
   {
     httpMethod: "POST",
-    body: {
+    body: JSON.stringify({
       batchId: 1,
       factoryId: "ABC",
       totalKwh: 10000,
       totalUnits: 1000,
       gridFactor: 1,
-    },
+    }),
   } as unknown as APIGatewayProxyEvent,
   {} as Context
 );
