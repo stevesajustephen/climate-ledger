cbe7be681edb04e5089e9336c2eadb30b1340c06
Steve Saju <stevesajustephen@gmail.com>
2026-01-16 08:55:10 +0100

    auth stack user pool and client

---
+++ src/infra/stacks/auth-stack.ts
@@ -0,0 +1,49 @@
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { OAuthScope, UserPool, UserPoolClient } from "aws-cdk-lib/aws-cognito";
+import { Construct } from "constructs";
+
+export class AuthStack extends Stack {
+  private userPool: UserPool;
+  private userPoolClient: UserPoolClient;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    this.createUserPool();
+    this.createUserPoolClient();
+  }
+
+  private createUserPool() {
+    this.userPool = new UserPool(this, "ClimateLedgerUserPool", {
+      userPoolName: "climate-ledger-users",
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+    });
+
+    this.userPool.addDomain("CognitoDomain", {
+      cognitoDomain: {
+        domainPrefix: "climate-ledger-auth-unique",
+      },
+    });
+
+    new CfnOutput(this, "ClimateLedgerUserPoolId", {
+      value: this.userPool.userPoolId,
+    });
+  }
+  private createUserPoolClient() {
+    this.userPoolClient = this.userPool.addClient("ClimateLedgerClient", {
+      userPoolClientName: "PartnerAppSupplier",
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true, // Gold standard for 2026
+        },
+        scopes: [OAuthScope.OPENID, OAuthScope.EMAIL, OAuthScope.PROFILE],
+        callbackUrls: ["http://localhost:5173/"], // Redirect here after login
+        logoutUrls: ["http://localhost:5173/"], // Redirect here after logout
+      },
+    });
+    new CfnOutput(this, "ClimateLedgerClientId", {
+      value: this.userPoolClient.userPoolClientId,
+    });
+  }
+}
